6a4f44b5f62e72fe8d6df7baf31f98e346e14227
Jonathan1295-09 <jcasasola955@gmail.com>
2023-10-15 21:40:45 -0500

    Index.js code written

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,22 @@
+import Post from '../components/Post';
+import { useRouteLoaderData, Form } from 'react-router-dom';
+
 function Index (props){
-    return <div>Index</div>
+    const projects = useRouteLoaderData()
+    return<>
+        <div>
+            <h2>Project Planner</h2>
+                <Form method='Post' action='/create'>
+                    <input type='text' name='Project name' placeholder='Project Name'/>
+                    <input type='date' name='Start Date' placeholder='Start Date'/>
+                    <input type='date' name='End Date' placeholder='End Date'/>
+                    <input type='text' name='Notes' placeholder='Notes'/>
+                    <input type='text' name='Supplies' placeholder='Supplies'/>
+                    <input type='Image' name='Project name' placeholder='Project Name'/>
+                </Form>
+        </div>
+        {projects.map((projects) => <Post key={projects.id} post={project}/>)}
+    </>
 }
 
 export default Index;
